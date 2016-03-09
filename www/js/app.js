f0597a389616c932b1cc5c4fc0df05e6fa2f5b47
Thiru <rthirucs@gmail.com>
2016-03-09 23:52:49 +0530

    Updated the app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,57 +1,37 @@
-// Ionic Starter App
+var app = angular.module('starter', ['ionic', 'ionic.service.core', 'starter.controllers', 'starter.services'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-var app = angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'starter.services'])
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
   })
 
-  // Each tab has its own nav history stack:
-    .state('tab.form', {
-      url: '/form',
-      templateUrl:'templates/form.html',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/form.html',
-          controller: 'FormCtrl'
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
+      .state('tab.form', {
+        url: '/form',
+        templateUrl: 'templates/form.html',
+        views: {
+          'tab-dash': {
+            templateUrl: 'templates/form.html',
+            controller: 'FormCtrl'
+          }
         }
-      }
-    })
+      });
 
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/form');
+    $urlRouterProvider.otherwise('/tab/form');
 
-});
+  });
